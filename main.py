575e4305d650a8f5910fcc583ff61d564212d365
yaxlie <m.lesnierowski@gmail.com>
2018-12-29 19:24:46 +0100

    Add results generation with genetics algorithm

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import problem_manager
 import genetics
 
-for s in [10, 20, 50, 100, 200, 500, 1000]:
+for s in [10, 20]: #, 50, 100, 200, 500, 1000]:
     print("n=" + str(s) + "\n")
     instances = problem_manager.read(s)
-    for i in instances:
-        problem_manager.schedule(i)
+    for i in range(len(instances)):
+        problem_manager.schedule(instances[i])
 
         # Testing genetic algorithm
-        genetics.start(i)
+        instances[i] = genetics.start(instances[i])
+
+    #     todo: dlaczego nie działa (czytelność) for job in instances:
+    #                                       job = genetics.start(job)
 
     for i in instances:
         problem_manager.export(i)
