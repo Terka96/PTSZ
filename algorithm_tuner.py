7b864f55a318db254dd27ec9fc8faa931c5f6c99
yaxlie <m.lesnierowski@gmail.com>
2019-01-05 02:37:04 +0100

    Update tuner UI

--- algorithm_tuner.py
+++ algorithm_tuner.py
@@ -5,23 +5,23 @@ import problem_manager
 #todo: generowanie list
 
 MAX_GENERATIONS = [3]    #numpy.arange(5, 10, 1)    # Index of last generation
-POPULATION_SIZE = [50]  #numpy.arange(20, 50, 10)   # Number of members in a generation
+POPULATION_SIZE = [500]  #numpy.arange(20, 50, 10)   # Number of members in a generation
 
-CONSTANT_MEMBERS = numpy.arange(0.1, 0.9, 0.3)
-CROSSOVER_CHANCE = numpy.arange(0.1, 0.9, 0.3)  # Chance for cross two members instead of just copy one of them to next generation
-MUTATION_CHANCE = numpy.arange(0.1, 0.9, 0.3)  # Chance for mutate child
+CONSTANT_MEMBERS = numpy.arange(0.1, 0.4, 0.3)
+CROSSOVER_CHANCE = numpy.arange(0.1, 0.4, 0.3)  # Chance for cross two members instead of just copy one of them to next generation
+MUTATION_CHANCE = numpy.arange(0.1, 0.4, 0.3)  # Chance for mutate child
 
 # Mutation parameters
-SWAP_ELEMENT_CHANCE = numpy.arange(0.1, 0.9, 0.3)  # Chance to swap the job
-CHANGE_R_CHANCE = numpy.arange(0.1, 0.9, 0.3)  # Chance to change R position
+SWAP_ELEMENT_CHANCE = numpy.arange(0.1, 0.4, 0.3)  # Chance to swap the job
+CHANGE_R_CHANCE = numpy.arange(0.1, 0.4, 0.3)  # Chance to change R position
 
 iterations = len(MAX_GENERATIONS) * len(CONSTANT_MEMBERS) * len(POPULATION_SIZE) * len(CROSSOVER_CHANCE) * len(MUTATION_CHANCE) * len(SWAP_ELEMENT_CHANCE) * len(CHANGE_R_CHANCE)
 
 ATTEMPTS = 5
 
-iter = 0
 #todo: wyniki muszą pochodzić z kilku pomiarów i być uśredniane
-for s in [10, 50, 100, 200]: #, 20, 50, 100, 200, 500, 1000]:
+for s in [10]: #, 20, 50, 100, 200, 500, 1000]:
+    iter = 0
     for max_gen in MAX_GENERATIONS:
         for pop_size in POPULATION_SIZE:
             for const_mem in CONSTANT_MEMBERS:
@@ -31,7 +31,7 @@ for s in [10, 50, 100, 200]: #, 20, 50, 100, 200, 500, 1000]:
                             for change_cha in CHANGE_R_CHANCE:
                                 instances_to_save = []
                                 iter += 1
-                                print("{0}/{1}".format(iter, iterations))
+                                print("\n{2}: {0}/{1}".format(iter, iterations, s))
                                 # todo: pasek postępu
                                 for x in range(ATTEMPTS):
                                     instances = problem_manager.read(s)
